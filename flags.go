6ff58e143fbaf68998eabbc7833603b906e448ec
Daryl Wright <daryl@goldenpath.ca>
2021-10-13 14:24:12 -0500

    Used the flags library handler for unknown app options.

--- flags.go
+++ flags.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/jessevdk/go-flags"
+	"os"
 )
 
 type TestOptions struct {
@@ -21,10 +24,18 @@ func GetAppFlags() (*TestOptions, *VersionOptions, error) {
 	testOptions := &TestOptions{}
 	versionOptions := &VersionOptions{}
 
-	// TODO: Reconsider enabling error printing as I may want to handle them myself.
-	//parser := flags.NewParser(testOptions, flags.PrintErrors | flags.PassDoubleDash)
-
 	parser := flags.NewNamedParser("ci", flags.Default)
+
+	parser.UnknownOptionHandler = func(option string, arg flags.SplitArgument, args []string) ([]string, error) {
+		parser.WriteHelp(os.Stdout)
+
+		err := flags.Error{
+			Type:    flags.ErrUnknownFlag,
+			Message: fmt.Sprintf("\nError: unknown flag '%v'", option),
+		}
+
+		return nil, errors.New(err.Error())
+	}
 
 	if _, err := parser.AddGroup("Test Group", "Long Group", testOptions); err != nil {
 		return nil, nil, err
@@ -35,114 +46,8 @@ func GetAppFlags() (*TestOptions, *VersionOptions, error) {
 	}
 
 	if _, err := parser.Parse(); err != nil {
-		switch flagsErr := err.(type) {
-		case flags.ErrorType:
-			if flagsErr == flags.ErrUnknownFlag {
-				//PrintHelpTextAndExit()
-			}
-		default:
-			return nil, nil, err
-		}
+		return nil, nil, err
 	}
 
 	return testOptions, versionOptions, nil
 }
-
-//func PrintHelpTextAndExit() {
-//	if helpText, err := GetHelpText(); err != nil {
-//		log.Fatal(err)
-//	} else if _, err = os.Stdout.WriteString(helpText); err != nil {
-//		log.Fatal(err)
-//	} else {
-//		os.Exit(0)
-//	}
-//}
-//
-//func GetHelpText() (string, error) {
-//	var out bytes.Buffer
-//
-//	writer := tabwriter.NewWriter(&out, 0, 2, 2, ' ', 0)
-//
-//	t := reflect.TypeOf(TestOptions{})
-//
-//	// TODO: Use the poorly documented 'flags' library functions to iterate on testOptions instead of using reflect.
-//	for _, fieldName := range []string{"Version", "Help", "Test1", "Test2", "Test3", "Test4", "Test5", "Test6"} {
-//		field, found := t.FieldByName(fieldName)
-//
-//		if !found {
-//			continue
-//		}
-//
-//		shortName := field.Tag.Get("short")
-//		longName := field.Tag.Get("long")
-//		description := field.Tag.Get("description")
-//		valueName := field.Tag.Get("value-name")
-//
-//		if field.Type.Kind() != reflect.Bool {
-//			if len(valueName) > 0 {
-//				shortName = fmt.Sprintf("-%v %v", shortName, valueName)
-//				longName = fmt.Sprintf("--%v=%v", longName, valueName)
-//			} else {
-//				shortName = fmt.Sprintf("-%v", shortName)
-//				longName = fmt.Sprintf("--%v=...", longName)
-//			}
-//		} else {
-//			shortName = fmt.Sprintf("-%v", shortName)
-//			longName = fmt.Sprintf("--%v", longName)
-//		}
-//
-//		maxOptionColWidth := 40
-//		maxDescriptionColWidth := 60
-//
-//		optionName := fmt.Sprintf("%v, %v", shortName, longName)
-//		optionName = wordwrap.WrapString(optionName, uint(maxOptionColWidth))
-//		optionLines := strings.Split(optionName, "\n")
-//
-//		description = wordwrap.WrapString(description, uint(maxDescriptionColWidth))
-//		descriptionLines := strings.Split(description, "\n")
-//
-//		if len(optionLines) > 0 && len(descriptionLines) > 0 {
-//			_, err := fmt.Fprintf(writer, "  %v\t%v\n", optionLines[0], descriptionLines[0])
-//
-//			if err != nil {
-//				return "", err
-//			}
-//
-//			// TODO: Need to iterate on the number of additional lines max(optionLines, descriptionLines) times.
-//			if len(optionLines) > 1 || len(descriptionLines) > 1 {
-//				var o, d string
-//
-//				if len(optionLines) > 1 {
-//					o = optionLines[1]
-//				} else {
-//					o = ""
-//				}
-//
-//				if len(descriptionLines) > 1 {
-//					d = descriptionLines[1]
-//				} else {
-//					d = ""
-//				}
-//
-//				_, err := fmt.Fprintf(writer, "  %v\t%v\n", o, d)
-//
-//				if err != nil {
-//					return "", err
-//				}
-//			}
-//		}
-//	}
-//
-//	err := writer.Flush()
-//
-//	if err != nil {
-//		return "", err
-//	}
-//
-//	// TODO: Make this application agnostic by removing references to specific programs.
-//	helpTextHeader := `Usage: ci [testOptions]
-//
-//Options:
-//`
-//	return helpTextHeader + out.String(), nil
-//}
